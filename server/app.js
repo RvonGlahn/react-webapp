58789a0b39236142bb59ac878ab6c434ff2a26fb
RvonGlahn <rasmusvong@gmx.de>
2021-05-09 12:15:44 +0200

    refactor: move constants from app.js to constants.js

--- server/app.js
+++ server/app.js
@@ -7,10 +7,10 @@ const logger = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
 const cors = require("cors");
-const rateLimit = require("express-rate-limit");
-const fs = require("fs");
 const indexRouter = require("./routes/index");
 const fifaRouter = require("./routes/fifa");
+const errorHandler = require("errorhandler");
+const constants = require("./constants");
 
 var app = express();
 
@@ -18,38 +18,15 @@ const port = process.env.PORT;
 const hostname = process.env.HOST;
 const csrfProtection = csrf({ cookie: true });
 
-// create a write stream (in append mode) for morgan logger
-const accessLogStream = fs.createWriteStream(
-    path.join(__dirname, "logs", "access.log"),
-    { flags: "a" }
-);
-
-// set access limiter to page
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-});
-
-// define cors options only for localhost and proxy
-const corsOptions = {
-    origin: [
-        "http://${process.env.HOST}:${process.env.PORT}/",
-        "${process.env.PROXY_URL}:${process.env.PROXY_PORT}/",
-    ],
-    methods: "GET,HEAD,POST",
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
-};
-
 // allow proxy for nginx
 app.set("trust proxy", true);
 
 // add middleware
-app.use(logger("combined", { stream: accessLogStream }));
+app.use(logger("combined", { stream: constants.accessLogStream }));
 app.use(helmet());
 app.use(compression());
-app.use(cors(corsOptions));
-app.use(limiter); //  apply limiter to all requests
+app.use(cors(constants.corsOptions));
+app.use(constants.limiter); //  apply limiter to all requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -60,9 +37,21 @@ app.use("/fifa21", fifaRouter);
 app.use("/", indexRouter);
 
 // error handler
-app.use(function (error, req, res, next) {
-    // response with the error message
-    res.json({ message: error.message });
+if (process.env.NODE_ENV === "development") {
+    app.use(
+        errorHandler({
+            dumpExceptions: true,
+            showStack: true,
+        })
+    );
+} else if (process.env.NODE_ENV === "production") {
+    app.use(errorHandler());
+}
+
+process.on("uncaughtException", function (err) {
+    console.error("An uncaught error occurred!");
+    console.error(err.stack);
+    process.exit();
 });
 
 app.listen(port, hostname, () => {
---
+++ server/constants.js
@@ -0,0 +1,30 @@
+const rateLimit = require("express-rate-limit");
+const fs = require("fs");
+const path = require("path");
+
+// create a write stream (in append mode) for morgan logger
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "logs", "access.log"),
+    { flags: "a" }
+);
+
+// set access limiter to page
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+});
+
+// define cors options only for localhost and proxy
+const corsOptions = {
+    origin: [
+        "http://${process.env.HOST}:${process.env.PORT}/",
+        "${process.env.PROXY_URL}:${process.env.PROXY_PORT}/",
+    ],
+    methods: "GET,HEAD,POST",
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+};
+
+exports.accessLogStream = accessLogStream;
+exports.limiter = limiter;
+exports.corsOptions = corsOptions;
