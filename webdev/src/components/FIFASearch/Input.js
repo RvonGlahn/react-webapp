0d63bc1f0b9e32ccf1ff7317486fd4406e58b90e
RvonGlahn <rasmusvong@gmx.de>
2021-05-22 08:54:22 +0200

    layout: add InputForm from bootstrap to Input.js

--- webdev/src/components/FIFASearch/Input.js
+++ webdev/src/components/FIFASearch/Input.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Input.css';
 import ButtonSubmit from '../Buttons/ButtonSubmit';
-import TextInput from './TextInput';
-import SelectInput from './SelectInput';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import Players from './Players';
 import { loadPlayer, loadSuggest, loadLists } from '../../services/fifa';
 
@@ -13,6 +14,8 @@ class Input extends React.Component {
             name: '',
             position: '',
             age: '',
+            player_value: '',
+            fifa_versions: [21, 20, 19, 18, 17, 16, 15],
             ability1Name: '',
             ability1Value: '',
             ability2Name: '',
@@ -76,13 +79,21 @@ class Input extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className="form-container">
-                    <form className="form" autoComplete="off">
-                        <h1 className="heading">Suche Spieler in FIFA 21</h1>
-                        <div className="suggest">
-                            <label className="form-label">Name</label>
-                            <input className="form-input form-inputs" list="suggest" name="name" wert="Name" onChange={this.handleChange} />
+            <Container style={{ paddingTop: '75px', paddingBottom: '100px' }}>
+                <h1 className="heading">Suche Spieler in FIFA 21</h1>
+                <Form>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridName">
+                            <Form.Label>Name</Form.Label>
+                            <input
+                                type="text"
+                                className="form-input"
+                                list="suggest"
+                                name="name"
+                                wert="Name"
+                                onChange={this.handleChange}
+                                autoComplete="new-password"
+                            />
                             <datalist id="suggest">
                                 {this.state.suggestion ? (
                                     this.state.suggestion.map((playerName, i) => <option value={playerName} key={i}></option>)
@@ -90,18 +101,83 @@ class Input extends React.Component {
                                     <option></option>
                                 )}
                             </datalist>
-                        </div>
-                        <SelectInput name="position" liste={this.state.posList} title="Position" onChange={this.handleChange} />
-                        <TextInput type="number" name="age" wert="Max. Alter" placeholder="0 - 99" onChange={this.handleChange} />
-                        <SelectInput name="ability1Name" liste={this.state.attrList} title="1. Fähigkeit" onChange={this.handleChange} />
-                        <TextInput type="number" name={'ability1Value'} wert="" placeholder="Minimum Wert (0 - 99)" onChange={this.handleChange} />
-                        <SelectInput name="ability2Name" liste={this.state.attrList} title="2. Fähigkeit" onChange={this.handleChange} />
-                        <TextInput type="number" name="ability2Value" wert="" placeholder="Minimum Wert (0 - 99)" onChange={this.handleChange} />
-                        <ButtonSubmit buttonStyle="btn--primary--form" buttonSize="btn--wide" buttonColor="blue" onClick={this.handleClick} />
-                    </form>
-                </div>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridPosition">
+                            <Form.Label>Position</Form.Label>
+                            <Form.Control as="select" name="position" value={this.state.position} onChange={this.handleChange}>
+                                {this.state['posList'].map((position, i) => (
+                                    <option value={position} key={i}>
+                                        {position}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridAttribute1">
+                            <Form.Label>Skill 1</Form.Label>
+                            <Form.Control as="select" name="ability1Name" value={this.state.ability1Name} onChange={this.handleChange}>
+                                {this.state['attrList'].map((position, i) => (
+                                    <option value={position} key={i}>
+                                        {position}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formGridAtt1Value">
+                            <Form.Label>Wert Skill 1</Form.Label>
+                            <Form.Control name="ability1Value" value={this.state.ability1Value} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridAttribute2">
+                            <Form.Label>Skill 2</Form.Label>
+                            <Form.Control as="select" name="ability2Name" value={this.state.ability2Name} onChange={this.handleChange}>
+                                {this.state['attrList'].map((position, i) => (
+                                    <option value={position} key={i}>
+                                        {position}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridAtt2Value">
+                            <Form.Label>Wert Skill 2</Form.Label>
+                            <Form.Control type="text" name="ability2Value" value={this.state.ability2Value} onChange={this.handleChange} />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridAge">
+                            <Form.Label>Maximales Alter</Form.Label>
+                            <Form.Control type="text" name="age" value={this.state.age} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridWert">
+                            <Form.Label>Spieler Wert</Form.Label>
+                            <Form.Control type="text" name="player_value" value={this.state.player_value} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridVersion">
+                            <Form.Label>FIFA Version</Form.Label>
+                            <Form.Control as="select" defaultValue="Choose...">
+                                {this.state['fifa_versions'].map((position, i) => (
+                                    <option value={position} key={i}>
+                                        FIFA {position}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Col></Col>
+                        <Col>
+                            <ButtonSubmit buttonStyle="btn--primary--form" buttonSize="btn--wide" buttonColor="blue" onClick={this.handleClick} />
+                        </Col>
+                        <Col></Col>
+                    </Form.Row>
+                </Form>
                 <div>{Object.keys(this.state.players).length !== 0 && <Players players={this.state.players} />}</div>
-            </div>
+            </Container>
         );
     }
 }
